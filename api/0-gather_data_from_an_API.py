0b8e4870441a1ac9869c2510f0216c46e32a3e9b
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-12 15:32:06 -0500

    task_0 Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    user_id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+
+    employee = requests.get(user_url + user_id).json()
+    total_task = requests.get(todos_url, params={"userId": user_id}).json()
+    done_task = requests.get(
+        todos_url, params={"userId": user_id, "completed": "true"}).json()
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(done_task), len(total_task)))
+
+    for task in done_task:
+        print("\t {}".format(task.get("title")))
