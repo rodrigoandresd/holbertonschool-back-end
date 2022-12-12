be96ab9e2c89befa7f82b64fd5db0cb7b5d2f057
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-12 17:11:14 -0500

    task 2. Export to JSON correction output

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -15,10 +15,8 @@ if __name__ == "__main__":
     employee = requests.get(user_url + user_id).json()
     username = employee.get("username")
     total_task = requests.get(todos_url, params={"userId": user_id}).json()
-    done_task = requests.get(
-        todos_url, params={"userId": user_id, "completed": "true"}).json()
 
     with open(user_id + ".json", "w") as json_file:
-        json.dump({user_id: [{"task": task.get("title"), "completed": task.get(
-            "completed"), "username": username} for task in total_task]},
+        json.dump({user_id: [{"task": t.get("title"), "completed": t.get(
+            "completed"), "username": username} for t in total_task]},
             json_file)
